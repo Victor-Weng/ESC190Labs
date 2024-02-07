d6ebd92a83c5e4d5da8cd8cca9f5fa03820e3889
Victor-Weng <victorwengvtw@gmail.com>
2024-02-07 16:55:53 -0500

    c

--- Lab04/Questions1to3.c
+++ Lab04/Questions1to3.c
@@ -24,12 +24,14 @@ void openfile()
     // Opening file in reading mode
     ptr = fopen("a.txt","r");
 
-    // Part d, error checking
-    
     if(NULL == ptr)
     {
         printf("file can't be opened \n");
     }
+
+    // Part d, error checking
+    
+    
     
 
     printf("content of this file are \n");
@@ -50,7 +52,7 @@ void openfile()
 int main()
 {
 
-    int temp = -1;
+    //int temp = -1;
     //segfault(temp);
     openfile();
     return 0;
